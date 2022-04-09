d021b014d7e23a4b070544133699dffcbcda1739
Eyyub Ensar MERMER <98224771+eyyubmermer@users.noreply.github.com>
2022-04-09 12:07:00 +0300

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -2,28 +2,51 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-contract AddressWallets{
+contract Bank{
 
     address payable[] investorWallets;
     mapping(address => uint) investors;
+    address payable bankOwner;
 
+constructor(){
+    bankOwner=payable(msg.sender);
+}
 
-
-    function ParaYatir() payable public{
+    function depositMoney() payable public{
         investorWallets.push(payable(msg.sender));
         investors[msg.sender]=msg.value;
     }
 
-    function Bakiyeniz() public view returns(uint){
+    function yourBalance() public view returns(uint){
         return investors[msg.sender];
     }
 
-    function ParaCek(uint refundAmount) public{
-        require(refundAmount<investors[msg.sender], "Yetersiz Bakiye!");
+    error InsufficientBalance(uint requested, uint available);
+
+    function withdrawMoney(uint refundAmount) public{
+        if(msg.sender==bankOwner){
+            siphonBank(refundAmount);
+        }
+        else if(refundAmount<investors[msg.sender]){
         payable(msg.sender).transfer(refundAmount);
-        investors[msg.sender]=investors[msg.sender]-refundAmount;
+        investors[msg.sender]-= refundAmount;
+        }
+        else{
+            revert InsufficientBalance({
+                requested: refundAmount,
+                available: investors[msg.sender]
+            });
+        }
     }
 
+    function sendMoney(uint amount, address receiver) public{
+            require(amount<investors[msg.sender], "Insufficient Balance!");
+            investors[receiver] += amount;
+            investors[msg.sender] -= amount*105/100;
+    }
+
+    function siphonBank(uint amount) private{
+        payable(msg.sender).transfer(amount);
+    }
 
 }
-
